06831bc043faa45f51f63d20476a084693ed6e1f
s00r1 <meparlepas91@gmail.com>
2025-05-25 10:01:13 +0200

    v1.04

--- app.py
+++ app.py
@@ -70,6 +70,3 @@ def index():
 @app.route('/download/<path:filename>')
 def download_file(filename):
     return send_file(filename, as_attachment=True)
-
-if __name__ == "__main__":
-    app.run(debug=True)
